964036f34decabd1e71934e571f2264576ca638b
Reavershark <jonasty1999@gmail.com>
2019-01-30 15:47:35 +0100

    Final version

---
+++ Assets/Scripts/HandInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using HTC.UnityPlugin.Vive;
+public class HandInput : MonoBehaviour
+{
+    public GameObject trackedCamera;
+
+    private void Update()
+    {
+        if (ViveInput.GetPressDown(HandRole.RightHand, ControllerButton.Menu))
+        {
+            if (trackedCamera.activeSelf)
+            {
+                trackedCamera.SetActive(false);
+            }
+            else {
+                trackedCamera.SetActive(true);
+            }
+        }
+    }
+}
