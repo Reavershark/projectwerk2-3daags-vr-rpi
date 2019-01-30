b4761c9e6bd1210f338dd872f91c027ec41ca01f
Reavershark <jonasty1999@gmail.com>
2019-01-30 12:56:02 +0100

    Initial commit

---
+++ Assets/Scripts/Noclip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noclip : MonoBehaviour {
+
+    public GameObject teleportField;
+    public GameObject enabledText;
+    
+    public void Enable()
+    {
+        teleportField.SetActive(true);
+        
+        enabledText.GetComponent<UnityEngine.UI.Text>().text = "Enabled";
+    }
+
+    public void Disable()
+    {
+        teleportField.SetActive(false);
+        enabledText.GetComponent<UnityEngine.UI.Text>().text = "Disabled";
+    }
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public GameObject teleporter;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void StartTeleport () {
+        player.GetComponent<Transform>().position = teleporter.GetComponent<Transform>().position;
+    }
+}
